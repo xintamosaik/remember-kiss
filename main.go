cfe2151d7d312ffa999d50818ca0c77563f70782
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-10-15 23:52:52 +0200

    move to golang

--- main.go
+++ main.go
@@ -4,27 +4,38 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 const PORT = ":8080"
 
+
+// This generates 3 static html files index.html, add.html and edit.html in the folder public
+func regenerateHTML() {
+	tmpl := template.Must(template.ParseFiles("frame.html"))
+	content := struct {
+		Title   string
+		Content template.HTML
+	}{
+		Title:   "TODO",
+		Content: template.HTML("<main>Hi</main>"),
+	}
+	// write to file
+	fs, err := os.Create("public/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fs.Close()
+	err = tmpl.ExecuteTemplate(fs, "frame", content)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
 
-	tmpl := template.Must(template.ParseFiles(
-		"frame.html",
-		"content.html",
-	))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]interface{}{
-			"Title":   "Hello world",
-			"Heading": "Welcome",
-			"Body":    "This content comes from layout.html",
-		}
 
-		if err := tmpl.ExecuteTemplate(w, "frame", data); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	regenerateHTML()
 
 	log.Println("Serving on http://localhost" + PORT)
 
