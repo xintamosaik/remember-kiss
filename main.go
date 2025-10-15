139e2ad99b1be86f093fc79c0c7c14c98985cd83
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-10-15 23:54:40 +0200

    autogenerate files

--- main.go
+++ main.go
@@ -9,30 +9,44 @@ import (
 
 const PORT = ":8080"
 
-
 // This generates 3 static html files index.html, add.html and edit.html in the folder public
 func regenerateHTML() {
 	tmpl := template.Must(template.ParseFiles("frame.html"))
-	content := struct {
-		Title   string
-		Content template.HTML
+
+	pages := []struct {
+		filename    string
+		title       string
+		contentFile string
 	}{
-		Title:   "TODO",
-		Content: template.HTML("<main>Hi</main>"),
+		{"public/index.html", "TODO", "main.html"},
+		{"public/add.html", "Add Item", "add.html"},
+		{"public/edit.html", "Edit Item", "edit.html"},
 	}
-	// write to file
-	fs, err := os.Create("public/index.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fs.Close()
-	err = tmpl.ExecuteTemplate(fs, "frame", content)
-	if err != nil {
-		log.Fatal(err)
+
+	for _, page := range pages {
+		contentBytes, err := os.ReadFile(page.contentFile)
+		if err != nil {
+			log.Fatalf("Failed to read %s: %v", page.contentFile, err)
+		}
+		content := struct {
+			Title   string
+			Content template.HTML
+		}{
+			Title:   page.title,
+			Content: template.HTML(string(contentBytes)),
+		}
+		fs, err := os.Create(page.filename)
+		if err != nil {
+			log.Fatalf("Failed to create %s: %v", page.filename, err)
+		}
+		err = tmpl.ExecuteTemplate(fs, "frame", content)
+		fs.Close()
+		if err != nil {
+			log.Fatalf("Failed to render %s: %v", page.filename, err)
+		}
 	}
 }
 func main() {
-
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	regenerateHTML()
